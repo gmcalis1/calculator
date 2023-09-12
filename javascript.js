116c9cf7a58fe37c9823178aa70564ec419e9388
gmcalis1 <gmcalister88@gmail.com>
2023-09-12 15:59:41 -0400

    Created button layouts and linked to js

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+const clear = document.querySelector('#clear');
+
+const enter = document.querySelector('#enter');
+const sign = document.querySelector('#sign');
+const output = document.querySelector('#outputLine');
+
+
+function equationCreation(){
+
+}
+
+function operate(){
+
+}
+
