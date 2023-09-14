c3e31a94c26cd8b004e3302e0a54c6153dc58054
Gabriel McAlister <gmcalister88@gmail.com>
2023-09-13 21:08:09 -0400

    Fiddling with functionalities

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+// Number Buttons
 const one = document.querySelector('#one');
 const two = document.querySelector('#two');
 const three = document.querySelector('#three');
@@ -8,39 +10,76 @@ const seven = document.querySelector('#seven');
 const eight = document.querySelector('#eight');
 const nine = document.querySelector('#nine');
 
+// Operator Buttons
 const add = document.querySelector('#add');
 const subtract = document.querySelector('#subtract');
 const multiply = document.querySelector('#multiply');
 const divide = document.querySelector('#divide');
-const clear = document.querySelector('#clear');
 
+
+// Function Buttons
+const clear = document.querySelector('#clear');
 const enter = document.querySelector('#enter');
 const sign = document.querySelector('#sign');
 const output = document.querySelector('#outputLine');
 
+let operator;
+let operatorChosen = false;
 let begin = true;
 const symbols = ['+', '-', '*', '/'];
+let answer;
+let num1;
+let num2;
+
+function operater(entry){
+    if()
+    operater = entry;
+    num1 = output.innerHTML
+    equationCreation(entry);
+}
+
+
 
 function equationCreation(input){
-    console.log(symbols.includes(input))
-    if(begin == true){
+
+    if(symbols.includes(input) && operatorChosen == true){
+        operate();
+    }
+
+    if(begin == true && !symbols.includes(input)){
         begin = false;
         output.innerHTML = input;
         return
-    }else if(symbols.includes(input) == "true" && begin == "true"){
-        alert("You must input a number other than 0 before you add an operator");
-        return
     }
 
-    output.innerHTML = output.innerHTML + " " + input;
+    if(symbols.includes(input)){
+        output.innerHTML = output.innerHTML + " " + input + " ";
+        return;
+    }
+
+    output.innerHTML = output.innerHTML + input;
 }
 
 function clearEverything(){
     output.innerHTML = 0;
+    answer = null;
     begin = true;
 }
 
 function operate(){
+    console.log(output.innerHTML); 
+    console.log(output.innerHTML.length);
+    let next = false;
+
+    for(i = 0; i < output.innerHTML.length; i++){
+        if(output.innerHTML.charAt(i) !== " "){
+            num1 = num1 + output.innerHTML.charAt(i);
+        }
+        
+        if(output.innerHTML.charAt(i+1) == " "){
+
+        }
+    }
 
 }
 
