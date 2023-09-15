964aab78d7493212d6a245141277172eb480074a
Gabriel McAlister <gmcalister88@gmail.com>
2023-09-15 15:53:08 -0400

    Main operator functionalities now working

--- javascript.js
+++ javascript.js
@@ -21,65 +21,105 @@ const divide = document.querySelector('#divide');
 const clear = document.querySelector('#clear');
 const enter = document.querySelector('#enter');
 const sign = document.querySelector('#sign');
-const output = document.querySelector('#outputLine');
+const decimal = document.querySelector('#decimal');
 
-let operator;
+// Output Lines
+const output = document.querySelector('#outputLine');
+const history = document.querySelector('#historyLine');
+
+let operator = '';
 let operatorChosen = false;
 let begin = true;
 const symbols = ['+', '-', '*', '/'];
-let answer;
-let num1;
-let num2;
+let answer = ''
+let num1 = ''
+let num2 = ''
 
-function operater(entry){
-    if()
-    operater = entry;
-    num1 = output.innerHTML
+
+function operatorFunction(entry){
+    if(operator !== ''){
+        history.innerHTML = output.innerHTML;
+        operate();
+        num2 = ''
+        num1 = answer;
+        output.innerHTML = output.innerHTML + ' ' + entry + ' ';
+        operator = entry;
+        return;
+    }
+    operator = entry;
     equationCreation(entry);
 }
 
 
 
 function equationCreation(input){
-
-    if(symbols.includes(input) && operatorChosen == true){
-        operate();
-    }
-
-    if(begin == true && !symbols.includes(input)){
+    if(begin == true){
         begin = false;
         output.innerHTML = input;
-        return
-    }
-
-    if(symbols.includes(input)){
-        output.innerHTML = output.innerHTML + " " + input + " ";
+        num1 = input;
         return;
     }
 
-    output.innerHTML = output.innerHTML + input;
+    if(symbols.includes(input)){
+        output.innerHTML = output.innerHTML + ' ' + input + ' ';
+        return;
+    }
+
+    if(begin == false && !symbols.includes(operator)){
+        output.innerHTML = output.innerHTML + input;
+        num1 = num1 + input;
+        return;
+    }
+    else if(begin == false && symbols.includes(operator)){
+        output.innerHTML = output.innerHTML + input;
+        num2 = num2 + input;
+        return;
+    }
 }
 
 function clearEverything(){
     output.innerHTML = 0;
-    answer = null;
+    operatorChosen = false;
+    history.innerHTML = '';
+    operator = '';
+    answer = '';
     begin = true;
+    num1 = '';
+    num2 = '';
 }
 
+//FIGURE OUT HOW WHY UNDEFINED IS GIVEN WHEN YOU PRESS ANOTHER OPERATOR AFTER YOU AN EQUATION
+
 function operate(){
-    console.log(output.innerHTML); 
-    console.log(output.innerHTML.length);
-    let next = false;
-
-    for(i = 0; i < output.innerHTML.length; i++){
-        if(output.innerHTML.charAt(i) !== " "){
-            num1 = num1 + output.innerHTML.charAt(i);
-        }
-        
-        if(output.innerHTML.charAt(i+1) == " "){
-
-        }
+    if(num1 == undefined){
+        history.innerHTML = output.innerHTML;
+        return;
     }
 
+    if(operator == '+'){
+        answer = Number(num1) + Number(num2);
+        history.innerHTML = output.innerHTML;
+        output.innerHTML = answer;
+    }
+    else if(operator == '-'){
+        answer = Number(num1) - Number(num2);
+        history.innerHTML = output.innerHTML;
+        output.innerHTML = answer;
+    }
+    else if(operator == '*'){
+        answer = Number(num1) * Number(num2);
+        history.innerHTML = output.innerHTML;
+        output.innerHTML = answer; 
+    }
+    else if(operator == '/'){
+        if(num2 == '0'){
+            alert("You cannot divide by 0!");
+            clearEverything();
+            return;
+        }
+        answer = Number(num1) / Number(num2);
+        history.innerHTML = output.innerHTML;
+        output.innerHTML = answer; 
+    }
 }
 
